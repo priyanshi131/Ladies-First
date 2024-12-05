362398e6c4d00cad66fdd52f9d17fba99777e21a
Priyanshi Acharya <117939163+priyanshi131@users.noreply.github.com>
2024-12-05 15:53:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const jwt=require("jsonwebtoken");
 const multer=require("multer");
 const path=require("path");
 const cors=require("cors");
+const { type } = require("os");
+const { request } = require("http");
+
 
 app.use(express.json());
 app.use(cors());
@@ -38,6 +41,141 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     })
 })
 
+// Schema for Creating Products
+
+const Product=mongoose.model("Product",{
+    id:{
+        type:Number,
+        required: true
+    },
+    name:{
+        type: String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    new_price:{
+        type:Number,
+        required:true
+    },
+    old_price:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+
+})
+
+app.post('/addproduct',async(req,res)=>{
+    let products=await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array=products.slice(-1);
+        let last_product=last_product_array[0];
+        id=last_product.id+1;
+    }else{
+        id=1;
+    }
+     const product=new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+     });
+     console.log(product);
+     await product.save();
+     console.log("Saved");
+     res.json({
+        success:true,
+        name:req.body.name,
+
+     })
+})
+
+// Creating API for deleting Products
+
+app.post('/removeproduct',async(req,res)=>{
+     await Product.findOneAndDelete({id:req.body.id});
+     console.log("Removed");
+     res.json({
+        success:true,
+        name:req.body.name
+     })
+})
+
+// Creating API for getting all products
+app.get('/allproducts',async(req,res)=>{
+    let products=await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
+
+//Schema creating for User model
+
+const Users=mongoose.model('Users',{
+    name:{
+        type:String
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+// Creating Endpoint for registering the user
+app.post('/signup',async (req,res)=>{
+    let check=await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+    }
+    let cart={};
+    for(let i=0;i<300;i++){
+        cart[i]=0;
+
+    }
+    const user=new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token=jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
 app.listen(port,(error)=>{
     if(!error){
         console.log("server running on port "+port);
